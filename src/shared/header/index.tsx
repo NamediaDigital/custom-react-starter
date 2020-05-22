cc43d38f14de0614e295bca3b59c334d1d83ac64
Res Yeg <shaqk702@gmail.com>
2020-05-21 21:07:01 -0700

    Start Header

---
+++ src/shared/header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Namedia Digital</h1>
+      <div>Get Started</div>
+    </div>
+  )
+}
+
+export default Header
